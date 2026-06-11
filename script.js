68879e3b94515d13c97999a32813b165bb43f36a
rogercan16-lab <rogercan16@gmail.com>
2026-06-11 02:00:49 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('start-btn').addEventListener('click', () => {
+    alert("¡Iniciando Cloverfield!");
+});
+
+document.getElementById('settings-btn').addEventListener('click', () => {
+    alert("Abriendo ajustes...");
+});
+
+document.getElementById('profile-btn').addEventListener('click', () => {
+    alert("Cargando perfil...");
+});
